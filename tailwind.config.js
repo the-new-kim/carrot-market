c2deb3c9d3e500a45e0140241eb0f19d234d1513
the-new-kim <jonkim0309@gmail.com>
2022-06-29 08:18:52 +0900

    pages, components, tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "media", // class
+  plugins: [require("@tailwindcss/forms")],
 };
